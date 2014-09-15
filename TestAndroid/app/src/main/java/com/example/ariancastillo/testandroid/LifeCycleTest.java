9d1c6b1bb69b05c39b977f3f8d531b6caa8fadec
Arian Castillo <caztle501@gmail.com>
2014-09-15 10:27:26 -0500

    Avance

--- TestAndroid/app/src/main/java/com/example/ariancastillo/testandroid/LifeCycleTest.java
+++ TestAndroid/app/src/main/java/com/example/ariancastillo/testandroid/LifeCycleTest.java
@@ -46,4 +46,3 @@ public class LifeCycleTest extends Activity {
     }
     
 }
-//SDK/extras/
--- TestAndroid/app/src/main/java/com/example/ariancastillo/testandroid/SurfaceViewTest.java
+++ TestAndroid/app/src/main/java/com/example/ariancastillo/testandroid/SurfaceViewTest.java
@@ -19,8 +19,7 @@ public class SurfaceViewTest extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         renderView = new FastRenderView(this);
         setContentView(renderView);
     }
